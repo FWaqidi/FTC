d9d79daab55784066bf53b2135388403e8626c65
Fateh Al Waqidi <FWaqidi@GMail.com>
2013-07-17 20:50:26 +0300

    Update MX.h

--- MX.h
+++ MX.h
@@ -2,6 +2,7 @@
 /Library/MobileSubstrate/DynamicLibraries/iap.plist|
 /var/mobile/Library/SBSettings/Toggles/iAPCracker/Toggle.dylib|
 /Applications/AppCrackr.app/AppCrackr|
-/Applications/SiNfuLCrack.app/SiNfuLCrack
+/Applications/SiNfuLCrack.app/SiNfuLCrack|
+/Applications/xxxx.app/xxxxx
 
 
