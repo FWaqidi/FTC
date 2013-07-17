4a6f71bbeb11c6461c51d98e10bea40be2dcf361
Fateh Al Waqidi <FWaqidi@GMail.com>
2013-07-18 01:56:10 +0300

    Update MX.h

--- MX.h
+++ MX.h
@@ -2,6 +2,10 @@
 /Library/MobileSubstrate/DynamicLibraries/iap.plist|
 /var/mobile/Library/SBSettings/Toggles/iAPCracker/Toggle.dylib|
 /Applications/AppCrackr.app/AppCrackr|
-/Applications/SiNfuLCrack.app/SiNfuLCrack
+/Applications/SiNfuLCrack.app/SiNfuLCrack|
+/Library/MobileSubstrate/DynamicLibraries/LocalIAPStore.dylib|
+/Library/MobileSubstrate/DynamicLibraries/LocalIAPStore.plist|
+/var/lib/dpkg/info/anondev.localiapstore.list|
+/Library/PreferenceLoader/Preferences/LocalIAPStore.plist
 
 
