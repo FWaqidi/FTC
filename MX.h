7e1bb2530fb673243da9a4c024e351506afa03c4
Fateh Al Waqidi <FWaqidi@GMail.com>
2013-07-17 05:19:24 +0300

    Update MX.h

--- MX.h
+++ MX.h
@@ -1,7 +1,7 @@
-/Library/MobileSubstrate/DynamicLibraries/iap.dylib
-/Library/MobileSubstrate/DynamicLibraries/iap.plist
-/var/mobile/Library/SBSettings/Toggles/iAPCracker/Toggle.dylib
-/Applications/AppCrackr.app/AppCrackr
+/Library/MobileSubstrate/DynamicLibraries/iap.dylib|
+/Library/MobileSubstrate/DynamicLibraries/iap.plist|
+/var/mobile/Library/SBSettings/Toggles/iAPCracker/Toggle.dylib|
+/Applications/AppCrackr.app/AppCrackr|
 /Applications/SiNfuLCrack.app/SiNfuLCrack
 
 
